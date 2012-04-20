59e2bdecc2afeb756a4f190a9e56a240d860ecbf
Diogo Eiras <eiras@outlook.com>
2012-04-20 16:49:44 +0000

    Weird zebras, they're crazy!

--- blackjack/blackjack/main.cpp
+++ blackjack/blackjack/main.cpp
@@ -11,6 +11,15 @@
 int main (int argc, const char * argv[])
 {
     std::cout << "primeiro commit!!";
+
+	for (int i = 0; true; ++i) {
+		cout << "They're fucking zebras!\n";
+
+		if (i >= 1000) {
+			cout << "That's enough!\n";
+			break;
+		}
+	}
     return 0;
 }
 
