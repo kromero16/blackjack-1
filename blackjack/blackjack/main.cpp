e7672722527d103a185328bb15e6600d5f499c8a
Pedro Fernandes <pedrorfernandes@yahoo.com>
2012-04-26 23:34:30 +0000

    main.cpp desapareceu do repo?

---
+++ blackjack/blackjack/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  blackjack
+//
+//  Created by pedro on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include "main.h"
+
+int main ()
+{
+    House a;
+    
+    a.populate();
+
+    
+    
+    return 0;
+}
+
