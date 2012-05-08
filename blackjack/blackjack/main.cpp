d28c2870cbb9d974ff09381cd9c3455fc8ebc6c4
Diogo Eiras <eiras@outlook.com>
2012-05-08 01:15:37 +0000

    Função para definir aposta mínima.

--- blackjack/blackjack/main.cpp
+++ blackjack/blackjack/main.cpp
@@ -45,12 +45,31 @@ vector<Player> getPlayers(){
     return vec_players;
 }
 
+int setApostaMinima(){
+	int aposta;
+
+	cout << "Defina aposta minima (valor inteiro positivo)" << endl << PROMPT;
+	cin >> aposta;
+
+	while(!cin.good() || aposta <= 0){
+		cout << "Por favor, atribua um valor plausivel (inteiro positivo) para a aposta minima" << endl << PROMPT;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin >> aposta;
+	}
+
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	return aposta;
+}
+
 int main ()
 {
     
     cout << "--------------- Blackjack --------------------" << endl;
     
-    Game* game = new Game( getPlayers() );
+    Game* game = new Game(getPlayers(), setApostaMinima());
     
     if ( !game->over() ) {
         game->playRound();
